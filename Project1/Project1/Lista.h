7f4f0edda607a83048e3a20a9c1d187b2c5d2e7e
Miguel-SS <55169938+Miguel-SS@users.noreply.github.com>
2020-09-21 23:27:59 -0600

    update

---
+++ Project1/Project1/Lista.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <sstream>
+
+class Lista {
+private:
+	struct Nodo {
+		int value;
+		struct Nodo* next;
+		struct Nodo* prev;
+		Nodo(int v = 0, Nodo* p = nullptr, Nodo* n = nullptr)
+			: value{ v }, prev{ p }, next{ n } {}
+	};
+	Nodo* inicio;
+
+public:
+	typedef Nodo* iterador;
+
+
+	Lista() { inicio = nullptr; }
+	Lista(const Lista&);
+	~Lista() { borrarLista(); }
+	void insertarFinal(int);
+	void eliminar(int);
+	void eliminarPrimero();
+	void borrarLista();
+	bool isVacia();
+	std::string toString();
+
+};
